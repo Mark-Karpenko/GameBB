fce8b2b475b58385ccfbdfc05b26f2f3db61ecd2
Mark-Karpenko <138462694+Mark-Karpenko@users.noreply.github.com>
2023-08-30 22:44:30 +0200

    SFML
    Include SFML library

--- DemoGame/main.cpp
+++ DemoGame/main.cpp
@@ -1,8 +1,20 @@
-#include<iostream>
+#include <SFML/Window.hpp>
 
 int main()
 {
-	std::cout << "Hello World" << std::endl;
+    sf::Window window(sf::VideoMode(800, 600), "My window");
 
-	return 0;
+    while (window.isOpen())
+    {
+
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+
+    return 0;
 }
